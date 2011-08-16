c14994bb71c3f99b1d3a23c0d2ae5e4eda6c9a96
Jimmy Liang <solo123@21cn.com>
2011-08-16 13:55:33 +0800

    resources

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,10 @@
 class Resource < ActiveRecord::Base
+  belongs_to :resource_data, :polymorphic => true
+  
+  scope :images, where(:file_type => 'img')
+  scope :softwares, where(:file_type => 'software')
+  scope :free, where(:resource_data_id => nil)
+  def self.icon
+    where(:file_type => 'icon').first
+  end  
 end
--- app/models/software.rb
+++ app/models/software.rb
@@ -1,4 +1,7 @@
 class Software < ActiveRecord::Base
   has_and_belongs_to_many :software_types
   has_and_belongs_to_many :mobile_types
+  
+  has_many :resources, :as => :resource_data, :dependent => :destroy
+  
 end
